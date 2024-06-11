7bfc237589b5fe600921af61ce8c400e6f41966e
Alex Massaro <alexander.massaro97@gmail.com>
2024-06-11 11:52:41 -0500

    Overriding Methods

--- Scripts/Characters/Player/PlayerController.cs
+++ Scripts/Characters/Player/PlayerController.cs
@@ -3,4 +3,14 @@ using System;
 
 public partial class PlayerController : CharacterBody3D
 {
+
+    public override void _Input(InputEvent @event)
+    {
+        GD.Print("Input");
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        GD.Print("Hello World");
+    }
 }
