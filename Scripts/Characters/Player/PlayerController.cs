d96e70cb49493886729df6e964c64b95330458f2
Alex Massaro <alexander.massaro97@gmail.com>
2024-06-11 12:13:34 -0500

    Exporting Fields with Attributes

--- Scripts/Characters/Player/PlayerController.cs
+++ Scripts/Characters/Player/PlayerController.cs
@@ -3,7 +3,23 @@ using System;
 
 public partial class PlayerController : CharacterBody3D
 {
+    [ExportGroup("Required Nodes")] // Groups all the Exported Attributes Below it into a Group in the Godot Editor
+    [Export] private Sprite3D characterSprite;
+    [Export] private AnimationPlayer animationPlayer;
     private Vector2 direction = new Vector2(0, 0);
+
+    public override void _Ready()
+    {
+        if (characterSprite != null)
+        {
+            GD.Print(characterSprite.Name);
+        }
+        if (animationPlayer != null)
+        {
+            GD.Print(animationPlayer.Name);
+        }
+    }
+
     public override void _Input(InputEvent @event) //This Method is called only when the player makes an input 
     {
         direction = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward"); //Take the Input from the Input Map
