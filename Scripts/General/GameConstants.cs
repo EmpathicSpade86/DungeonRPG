79ef6235992f8fa898ddf89664302068f26703da
Alex Massaro <alexander.massaro97@gmail.com>
2024-06-26 11:58:32 -0500

    Designing an Enemy

--- Scripts/General/GameConstants.cs
+++ Scripts/General/GameConstants.cs
@@ -3,7 +3,7 @@ public class GameConstants
 {
     //Animations
     public const string ANIM_IDLE = "Idle";
-    public const string ANIM_MOVE = "Running";
+    public const string ANIM_MOVE = "Move";
     public const string ANIM_DASH = "Dash";
     //Input
     public const string INPUT_MOVE_LEFT = "MoveLeft";
