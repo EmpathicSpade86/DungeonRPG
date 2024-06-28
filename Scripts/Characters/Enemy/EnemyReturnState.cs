f7a19633a9cca39fbdbbd8c0acb4744608f07b8c
Alex Massaro <alexander.massaro97@gmail.com>
2024-06-28 12:07:49 -0500

    Moving the Enemy

--- Scripts/Characters/Enemy/EnemyReturnState.cs
+++ Scripts/Characters/Enemy/EnemyReturnState.cs
@@ -10,21 +10,26 @@ public partial class EnemyReturnState : EnemyState
         base._Ready();
         Vector3 localPos = characterNode.PathNode.Curve.GetPointPosition(0); //Get's the first point of the PathNode's Curve and sets it to the destination
         Vector3 globalPos = characterNode.PathNode.GlobalPosition;
-        destination = localPos + globalPos; //Sets it so that we are using the global position, not the local position
+        destination = globalPos + localPos; //Sets it so that we are using the global position, not the local position
     }
 
     protected override void EnterState()
     {
-        base.EnterState();
         characterNode.AnimationPlayerNode.Play(GameConstants.ANIM_MOVE);
-
-
-        characterNode.GlobalPosition = destination; 
         SetPhysicsProcess(true);
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        
+        if (characterNode.GlobalPosition == destination)
+        {
+            //If the Enemy arrives at the destination
+            GD.Print("Reached Destination");
+            return;
+        }
+
+        characterNode.GlobalPosition.DirectionTo(destination); //Calculates the direction to move the object to
+
+        characterNode.MoveAndSlide();
     }
 }
