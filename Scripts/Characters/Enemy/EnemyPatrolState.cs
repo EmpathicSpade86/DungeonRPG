c5403f83857c0227a71ed7fa1f69a9ee7d636581
Alex Massaro <alexander.massaro97@gmail.com>
2024-07-01 11:32:05 -0500

    Pausing on Patrols

--- Scripts/Characters/Enemy/EnemyPatrolState.cs
+++ Scripts/Characters/Enemy/EnemyPatrolState.cs
@@ -3,7 +3,11 @@ using System;
 
 public partial class EnemyPatrolState : EnemyState
 {
+    [Export] private Timer IdleTimerNode; //Timer for pausing
+    [Export(PropertyHint.Range, "0,20,0.1")] private float maxIdleTime = 4.0f;
     private int pointIndex = 0;
+
+
     protected override void EnterState()
     {
         characterNode.AnimationPlayerNode.Play(GameConstants.ANIM_MOVE);
@@ -15,19 +19,38 @@ public partial class EnemyPatrolState : EnemyState
         characterNode.AgentNode.TargetPosition = destination;
 
         characterNode.AgentNode.NavigationFinished += HandleNavigationFinished;
+
+        IdleTimerNode.Timeout += HandleTimeout;
     }
+
     public override void _PhysicsProcess(double delta)
     {
+        //If the Timer is running
+        if (!IdleTimerNode.IsStopped())
+        {
+            //Then Do nothing
+            return;
+        }
         Move();
     }
 
     private void HandleNavigationFinished()
     {
+        //Pause at the Location for a few seconds
+        characterNode.AnimationPlayerNode.Play(GameConstants.ANIM_IDLE);
+        RandomNumberGenerator rng = new RandomNumberGenerator();
+        IdleTimerNode.WaitTime = rng.RandfRange(0, maxIdleTime);
+        IdleTimerNode.Start();
+    }
+
+    private void HandleTimeout()
+    {
+        characterNode.AnimationPlayerNode.Play(GameConstants.ANIM_MOVE);
         //Calculate the next position
         pointIndex = Mathf.Wrap(pointIndex + 1, 0, characterNode.PathNode.Curve.PointCount);
 
         //Move the Character to the new destination
-        destination = GetPointGlobalPosition(pointIndex); 
-        characterNode.AgentNode.TargetPosition = destination; 
+        destination = GetPointGlobalPosition(pointIndex);
+        characterNode.AgentNode.TargetPosition = destination;
     }
 }
