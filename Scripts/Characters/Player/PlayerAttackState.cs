93cb899ca10f9207a7655374ccba30d4807a408b
Alex Massaro <alexander.massaro97@gmail.com>
2024-07-08 14:26:27 -0500

    Combo Counters

--- Scripts/Characters/Player/PlayerAttackState.cs
+++ Scripts/Characters/Player/PlayerAttackState.cs
@@ -3,5 +3,27 @@ using System;
 
 public partial class PlayerAttackState : PlayerState
 {
-    
+    // Since the Player has 2 Attacks, we will need to switch between them
+    private int comboCounter = 1;
+    private int maxComboCount = 2;
+
+    protected override void EnterState()
+    {
+        characterNode.AnimationPlayerNode.Play(GameConstants.ANIM_ATTACK + comboCounter); //This is how we're going to play the different animations, Player's attack animations are labeled 'Attack1' and 'Attack2'
+        characterNode.AnimationPlayerNode.AnimationFinished += HandleAnimationFinished;
+    }
+
+    protected override void ExitState()
+    {
+        // Unsubscribe from the HandleAnimationFinished Signal
+        characterNode.AnimationPlayerNode.AnimationFinished -= HandleAnimationFinished;
+    }
+
+    private void HandleAnimationFinished(StringName animName)
+    {
+        comboCounter++; 
+        comboCounter = Mathf.Wrap(comboCounter, 1, maxComboCount + 1);
+
+        characterNode.StateMachineNode.SwitchState<PlayerIdleState>();
+    }
 }
--- Scripts/Characters/Player/PlayerState.cs
+++ Scripts/Characters/Player/PlayerState.cs
@@ -6,5 +6,12 @@ using System;
 //Must Define a Child Class to use this class
 public abstract partial class PlayerState : CharacterState
 {
+    protected void CheckForAttackInput()
+    {
+        if (Input.IsActionJustPressed(GameConstants.INPUT_ATTACK))
+        {
+            characterNode.StateMachineNode.SwitchState<PlayerAttackState>();
+        }
+    }
 
 }
