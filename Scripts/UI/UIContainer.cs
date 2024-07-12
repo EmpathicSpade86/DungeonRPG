011fe3abc2059b90c93052c920a962eb00a19adc
Alex Massaro <alexander.massaro97@gmail.com>
2024-07-12 12:18:29 -0500

    Grabbing UI Containers

---
+++ Scripts/UI/UIContainer.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public partial class UIContainer : VBoxContainer
+{
+    [Export] public ContainerType container { get; set; }
+
+
+}
---
+++ Scripts/UI/UIController.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public partial class UIController : Control
+{
+    private Dictionary<ContainerType, UIContainer> containers;
+
+    public override void _Ready()
+    {
+        containers = GetChildren().Where((element) => element is UIContainer).Cast<UIContainer>().ToDictionary((element) => element.container);
+
+        /**
+            GetChildren(): This method gets all children of the UIController node.
+            Where((element) => element is UIContainer): This LINQ method filters the children to only those that are of type UIContainer.
+            Cast<UIContainer>(): This casts the filtered elements to type UIContainer.
+            ToDictionary((element) => element.container): This converts the cast elements into a dictionary, using the container property of each UIContainer as the key.
+        **/
+        containers[ContainerType.Start].Visible = true;
+    }
+
+
+}
